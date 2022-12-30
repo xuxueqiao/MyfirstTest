ca939bacb0b2a65ea9ac7265d11fba7cd4b63282
xjxu <xjxu@dfhx123.com>
2022-12-30 17:11:31 +0800

    第一次提交

---
+++ GithubTest/src/Test01.java
@@ -0,0 +1,10 @@
+/**
+ * @author xuxueqiao
+ * @create 2022-12-30-17:07
+ **/
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
